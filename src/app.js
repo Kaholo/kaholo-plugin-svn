d52c6f12cac1ebe83d6f1625651d6a83eb0640ee
vinfry <mercurial.sp@gmail.com>
2019-03-21 11:00:45 +0200

    added getrevision method

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 var svn = require ("node-svn-ultimate");
 
+function _options(action, settings){
+    return {
+        username: action.params.USER || settings.USER,
+        password: action.params.PASSWORD || settings.PASSWORD
+    }
+}
+
 function checkOut(action, settings) {
     return new Promise((resolve,reject) => {
         let url = action.params.remote_SVN_repo;
         let wc = action.params.local_wc;
-        let login = {
-            username: action.params.USER || settings.USER,
-            password: action.params.PASSWORD || settings.PASSWORD
-        };
+        let options = _options(action, settings)
         svn.commands.co(url, wc, login, function (err, res) {
             if (err)
                 return reject(err);
@@ -18,7 +22,20 @@ function checkOut(action, settings) {
 
 }
 
+function getRevision(action, settings) {
+    return new Promise((resolve, reject) => {
+        let target = action.params.TARGET;
+        let options = _options(action, settings)
+        svn.commands.info(target, options, function (err, res) {
+            if (err)
+                return reject(err)
+            resolve(res.entry.$.revision)
+        })
+    })
+}
+
 
 module.exports = {
-    checkOut: checkOut
+    checkOut: checkOut,
+    getRevision: getRevision
 };
